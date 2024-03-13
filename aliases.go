553f5f84923cac81477a3eb328a42f1a5569e78d
Eugen <amberpixels.io@gmail.com>
2024-03-13 23:41:26 +0200

    add todos for aliases

--- aliases.go
+++ aliases.go
@@ -21,4 +21,21 @@ var builtinTimeAliases = map[string]func(time.Time) time.Time{
 		New(&base).TruncateToDay()
 		return base
 	},
+	// TODO:
+	"this-week": nil,
+	"last-week": nil,
+	"next-week": nil,
+
+	"this-weekend": nil,
+	"last-weekend": nil,
+
+	"this-month": nil,
+	"last-month": nil,
+	"next-month": nil,
+
+	// todo: quarter
+
+	"this-year": nil,
+	"last-year": nil,
+	"next-year": nil,
 }
