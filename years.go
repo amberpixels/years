36bdadd14d9a355b6fadfaa459f9cdba5621e67e
Eugene <eugene@amberpixels.io>
2025-03-13 20:25:56 +0200

    JustParse as public func

--- years.go
+++ years.go
@@ -8,3 +8,8 @@ import (
 func Parse(layout string, value string) (time.Time, error) {
 	return DefaultParser().Parse(layout, value)
 }
+
+// JustParse calls JustParse of a default parser.
+func JustParse(value string) (time.Time, error) {
+	return DefaultParser().JustParse(value)
+}
